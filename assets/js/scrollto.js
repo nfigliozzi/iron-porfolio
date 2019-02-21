2b2e3a2b9efa31ed70f52bde8d9cf18164abffad
P Nicolás Figliozzi <hola@nicolasfigliozzi.com.ar>
2019-02-21 16:04:27 +0100

    Agregar estilos responsive

--- assets/js/scrollto.js
+++ assets/js/scrollto.js
@@ -3,13 +3,24 @@
 var main = document.getElementsByTagName('main');
 main = main[0];
 var link = document.getElementById('scroll-indicator');
+var backlink = document.getElementById('scroll-back');
 
 link.onclick = function(e)
   { 
     e.preventDefault();
-    main.scrollTo(window.innerWidth / 2 , 0);
+    main.scrollTo(window.innerWidth * 2 , 0);
     link.className = " ";
+    backlink.className = "show";
   };
 
-link.className = "show";
+
+backlink.onclick = function(e)
+  { 
+    e.preventDefault();
+    main.scrollTo(0 , 0);
+    backlink.className = " ";
+    link.className = "show";
+  };
+
+  link.className = "show";
 })();
